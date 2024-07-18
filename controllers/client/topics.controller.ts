fef7990de16707f8ee1f7e1255368afaf1e796c0
Xuan Nam <leeminhnam2k2@gmail.com>
2024-07-18 12:40:05 +0700

    update topic page

--- controllers/client/topics.controller.ts
+++ controllers/client/topics.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express"
+import Topic from "../../model/topic.model"
 
-export const index = (req: Request, res: Response) => {
+export const index = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  })
+
   res.render("client/pages/topics/index", {
     pageTitle: "Topic",
+    topics: topics
   })
 }
---
+++ model/topic.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const topicSchema = new mongoose.Schema({
+  title: String,
+  avatar: String,
+  description: String,
+  status: String,
+  slug: String,
+  deleted: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+const Topic = mongoose.model("Topic", topicSchema, "topics");
+
+export default Topic;
